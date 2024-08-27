f9434b56586ba9fa73e3448383b9b3b68d939b83
jinyeong <wlsehfdl95@naver.com>
2024-08-27 17:26:38 +0900

    java basic 240827

---
+++ 01/java-start-mine/src/HelloJava.java
@@ -0,0 +1,9 @@
+public class HelloJava {  // 클래스명은 파일명과 같아야 함
+
+    public static void main(String[] args) {
+        System.out.println("hello java");
+    }
+}
+
+// 들여쓰기를 하지 않아도 프로그램은 작동하지만
+// 코드의 가독성을 위해 들여쓰기를 사용
